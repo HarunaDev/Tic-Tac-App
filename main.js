7b026e14fe1a52d33ace107fb9c20149a52e9f77
Haruna Alvin <harunaalvin69@gmail.com>
2022-02-16 21:53:45 +0100

    set app winning conditions

--- main.js
+++ main.js
@@ -9,6 +9,31 @@ let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let isGameActive = true;
 
+// varibles to announce end game results
+const playerxWon = 'PLAYER_X_WON';
+const playeroWon = 'PLAYER_O_WON';
+const tie = 'TIE';
+
+
+// winning conditions
+/* 
+Indexes within the board 
+[0] [1] [2]
+[3] [4] [5]
+[6] [7] [8]
+*/
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 // tile check function 
 const isValidaction = (tile) => {
     if (tile.innerText === 'X' || tile.innerText === 'O'){
