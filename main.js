d6d977977aec4fa87e1767382e6877cf20f9f09c
Haruna Alvin <harunaalvin69@gmail.com>
2022-02-16 21:37:15 +0100

    added game  functions to app

--- main.js
+++ main.js
@@ -8,3 +8,36 @@ const resetButton = document.getElementById('reset');
 let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let isGameActive = true;
+
+// player action function
+const userAction = (tile, index) => {
+    if(isValidaction(tile) && isGameActive) {
+        tile.innerText = currentPlayer;
+        tile.classList.add(`player${currentPlayer}`);
+        updateBoard(index);
+        handleResultValidation();
+        changePlayer();
+    }
+}
+
+// reset board function 
+const resetBoard = () => {
+    board = ['', '', '', '', '', '', '', '', ''];
+    isGameActive = true;
+    announcer.classList.add('hide');
+
+    if (currentPlayer === 'O') {
+        changePlayer();
+    }
+
+    tiles.forEach(tile => {
+        tile.innerText = '';
+        tile.classList.remove('playerX');
+        tile.classList.remove('playerO');
+    });
+}
+
+// add event listener to individual box
+tiles.forEach( (tile, index) => {
+    tile.addEventListener('click', () => userAction(tile, index));
+})
